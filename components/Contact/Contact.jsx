b79398bc6a360ef38d101da9b78b488ae6be04d1
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2022-10-26 20:02:22 +0100

    Emailjs integration

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,4 +1,28 @@
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
+
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_35gr5gc',
+        'template_3ik942r',
+        form.current,
+        'HcyMoj0YftLbS4Kso'
+      )
+      .then(
+        () => {
+          alert(`Sent!`);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <section
       id='contact'
@@ -9,18 +33,23 @@ function Contact() {
       </h3>
       <div className='md:mr-[-20%] mb-24'>
         <h4 className='text-white text-2xl mb-9'>Get in Touch</h4>
-        <form className='grid gap-12 text-gray-400' action='post'>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className='grid gap-12 text-gray-400'
+          action='post'
+        >
           <legend className='flex flex-wrap gap-10'>
             <input type='text' name='firstName' placeholder='Firstname' />
-            <input type='text' name='firstName' placeholder='Lastname' />
+            <input type='text' name='lastName' placeholder='Lastname' />
           </legend>
           <input
             className='w-fit'
             type='text'
-            name='firstName'
+            name='email'
             placeholder='Email Address'
           />
-          <input type='text' name='firstName' placeholder='Message' />
+          <input type='text' name='message' placeholder='Message' />
           <button type='submit' className='text-left outline-btn'>
             Send
           </button>
