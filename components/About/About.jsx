8bae3c8f530eccc0eebeee9e3a2087cb414d1f55
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2022-10-31 20:57:12 +0100

    responsiveness

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -4,30 +4,33 @@ function About() {
   return (
     <section id='about' className='bg-lighterBlue container-xy'>
       <h2 className='text-left subheading text-black'>
-        Here to Make Your Websites Look Elegant And Stylish
+        Here to Provide the Best Digital Experience for Your Brand
       </h2>
-      <div className='flex justify-between'>
-        {/* <Image
-          src={'/profile_image.png'}
-          alt='Peter'
-          width={463}
-          height={481}
-        /> */}
+      <div className='flex flex-col lg:flex-row gap-12'>
+        <div className='relative w-[150px] h-[150px] lg:w-[663px] lg:h-[250px]'>
+          <Image src={'/profile_image.png'} alt='Peter' layout='fill' />
+        </div>
         <div className='text-sm [&>p]:mb-4'>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-            assumenda explicabo quidem voluptate?
+            My name is Ogbonnaya Peter. A 19 years old, currently living in
+            Nigeria. I am a self-taught Developer
           </p>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            culpa perferendis enim architecto nulla consequatur doloremque quas,
-            vel voluptate sit.
+            I am a Solution Provider, who loves building interactive
+            user-interfaces that run on the web using modern technologies.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum
-            quasi beatae ipsam excepturi, fuga, praesentium dicta nesciunt qui
-            perspiciatis totam repellendus id voluptates quia cumque nihil saepe
-            a voluptate nam.
+            I enjoy the constant change in the technology used in the area and
+            love diving deeper into new frameworks and technologies. As an
+            active learner, my openess to new ideas helps me to focus and
+            discover new approaches to solving a problem and actually learn from
+            it. <br />
+            As a strong advocate of collaboration who believes that no one has
+            all the answers, I have made{' '}
+            <span className='text-lightBlue'>contributions</span> to good open
+            source projects and love what I learn in the process. <br /> I would
+            love to have a conversation about how I could be asset that&apos;ll
+            create great web experience for your brand
           </p>
           <p className='pt-4'>Quality Service and Support</p>
           <p>LifeTime Online Support</p>
