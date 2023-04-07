1b733500ec038a374b9a9f6c14a9999bcdd10c9f
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2023-04-07 02:28:07 +0100

    Social media app added

--- data/data.js
+++ data/data.js
@@ -1,65 +1,78 @@
 export const projects = [
   {
-    title: 'Groovy',
+    title: "Groovy",
     url: {
-      github: 'https://github.com/iampitrus/groovy',
-      live: 'https://groovy-os.vercel.app/',
-      image: '/groovy.png',
+      github: "https://github.com/iampitrus/groovy",
+      live: "https://groovy-os.vercel.app/",
+      image: "/groovy.png",
     },
     id: 1,
     description:
-      'An open source music player on the web (ongoing). As a contributor, I built different components like the floating player controls and made it responsive',
-    stack: 'Next.js, Shazam Api, Tailwind CSS and Material UI',
+      "An open source music player on the web (ongoing). As a contributor, I built different components like the floating player controls and made it responsive",
+    stack: "Next.js, Shazam Api, Tailwind CSS and Material UI",
   },
 
   {
-    title: 'Google Search clone',
+    title: "My Social Media App",
     url: {
-      github: 'https://github.com/iampitrus/Google-Search-clone',
-      live: 'http://google-clone-5sxy.vercel.app/',
-      image: '',
+      github: "https://github.com/iampitrus/MUI-App",
+      live: "https://mui-app-rouge.vercel.app/",
+      image: "/muiapp.png",
+    },
+    id: 6,
+    description:
+      "A social media app built with Material UI from ground up. The App is mobile responsive and has a switch between dark mode and light mode",
+    stack: "Next.js, Material UI",
+  },
+
+  {
+    title: "Google Search clone",
+    url: {
+      github: "https://github.com/iampitrus/Google-Search-clone",
+      live: "http://google-clone-5sxy.vercel.app/",
+      image: "",
     },
     id: 2,
     description:
-      'A clone to Google search, where you make searches and receive related websites, news, images and videos that match your search',
-    stack: 'React.js, React Context, Tailwind Css, Rapid Api',
+      "A clone to Google search, where you make searches and receive related websites, news, images and videos that match your search",
+    stack: "React.js, React Context, Tailwind Css, Rapid Api",
   },
 
   {
-    title: 'Sneakers',
+    title: "Sneakers",
     url: {
-      github: 'https://github.com/iampitrus/ecommerce-landing-page',
-      live: 'https://ecommerce-landing-page2.netlify.app/',
-      image: '/sneakers.png',
+      github: "https://github.com/iampitrus/ecommerce-landing-page",
+      live: "https://ecommerce-landing-page2.netlify.app/",
+      image: "/sneakers.png",
     },
     id: 3,
 
     description:
-      'A website where you can shop footwears of different kind both for men and women',
-    stack: 'React.js, Styled components',
+      "A website where you can shop footwears of different kind both for men and women",
+    stack: "React.js, Styled components",
   },
   {
-    title: 'Note Keeper',
+    title: "Note Keeper",
     url: {
-      github: 'https://github.com/iampitrus/Note-Keeper-App',
-      live: 'http://note-keeper-app-3ts4.vercel.app/',
-      image: '/keeper.png',
+      github: "https://github.com/iampitrus/Note-Keeper-App",
+      live: "http://note-keeper-app-3ts4.vercel.app/",
+      image: "/keeper.png",
     },
     id: 4,
     description:
-      'A website where you store notes from your thoughts, it comes with smooth layout animatinos',
-    stack: 'React.js, Framer-motion and Material UI',
+      "A website where you store notes from your thoughts, it comes with smooth layout animatinos",
+    stack: "React.js, Framer-motion and Material UI",
   },
   {
-    title: 'My Portfolio',
+    title: "My Portfolio",
     url: {
-      github: 'https://github.com/iampitrus/My-portfolio',
-      live: '',
-      image: '/portfolio.png',
+      github: "https://github.com/iampitrus/My-portfolio",
+      live: "/",
+      image: "/portfolio.png",
     },
     id: 5,
-    description: 'My web Portfolio',
-    stack: 'Next.js, Tailwind Css',
+    description: "My web Portfolio",
+    stack: "Next.js, Tailwind Css",
   },
 ];
 
